6ab65d33e74bd024d37ba645f1c2131572d2f989
kisuisou <irwinlex@yahoo.co.jp>
2022-03-18 16:44:00 +0900

    変数名の変更及びグラフ描画用の引数をmapメソッドを用いた実装に変更

--- analyze.rb
+++ analyze.rb
@@ -13,12 +13,12 @@ Dir.foreach(IMG_DIR_PATH) do |path|
 end
 imgs_exif.sort_by!{|x|x.focal_length_in_35mm_film.to_i}
 imgs_exif.each do |exif_data|
-  focal_length = exif_data.focal_length_in_35mm_film.to_i
-  array_tmp = imgs_chart_args.map{|length|length[0]}
-  if array_tmp.include?(focal_length)
-    imgs_chart_args[imgs_chart_args.length-1] = [focal_length,imgs_chart_args[imgs_chart_args.length-1][1]+1] 
+  fl = exif_data.focal_length_in_35mm_film.to_i
+  fl_ary_now_exists = imgs_chart_args.transpose[0]
+  if fl_ary_now_exists != nil && fl_ary_now_exists.include?(fl)
+    imgs_chart_args.map!{|len_data|len_data[0]==fl ? [fl,len_data[1]+1] : len_data}
   else
-    imgs_chart_args.push([focal_length,1])
+    imgs_chart_args.push([fl,1])
   end
 end
 puts AsciiCharts::Cartesian.new(imgs_chart_args,:bar => true, :hide_zero => true).draw
